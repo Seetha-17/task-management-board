d091cc9407a5ee2db745a0ee592f7d47ab7759bd
Seetha-17 <seethathummepalli17@gmail.com>
2025-09-03 11:37:28 +0530

    Delete task/task-management-board directory

--- task/task-management-board/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskBoardComponent } from './components/task-board/task-board.component';
-
-export const routes: Routes = [
-  { path: '', component: TaskBoardComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
-];
--- task/task-management-board/src/app/validators/no-whitespace.validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-export function noWhitespaceValidator(
-  control: AbstractControl
-): ValidationErrors | null {
-  return control.value.length && control.value.trim().length === 0
-    ? { whitespace: true }
-    : null;
-}
